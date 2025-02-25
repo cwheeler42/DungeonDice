720aa5ea727b6490a5be9c45f179c18226921c85
Chris Wheeler <cwheeler@emeals.com>
2025-02-25 16:22:51 -0500

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Chris Wheeler on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
